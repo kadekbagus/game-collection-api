03c491fb7198d90b2612417de3cf0782f143b350
kadek <kadek@dominopos.com>
2017-09-18 10:57:59 +0800

    remove unnecessary code

--- app.py
+++ app.py
@@ -4,27 +4,6 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    },
-    {
-        'id': 3,
-        'title': u'Get Laid',
-        'description': u'Find a beautiful girl and get laid',
-        'done': False
-    }
-]
-
 # mysql config
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
